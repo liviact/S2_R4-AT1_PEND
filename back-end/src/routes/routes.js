45247a56dab033a789f67d90c46561fb85233d73
DANIEL ALMEIDA LOVAGLIO <daniel.lovaglio2@senaisp.edu.br>
2026-05-15 13:04:32 -0300

    criado as rotas

--- back-end/src/routes/routes.js
+++ back-end/src/routes/routes.js
@@ -2,9 +2,10 @@ import { Router } from "express";
 const routes = Router();
 
 import produtoRoutes from "./produtoRoutes.js";
+import pedidoRoutes from "./pedidosRoutes.js";
 
 
 routes.use('/produtos', produtoRoutes)
-
+routes.use('/pedidos', pedidoRoutes)
 
 export default routes
