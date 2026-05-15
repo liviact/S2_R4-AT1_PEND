19e35cb5db51db297c2202bafaf43c26981ad1a7
NICOLAS GABRIEL COUTINHO <nicolas.coutinho3@senaisp.edu.br>
2026-05-15 13:43:11 -0300

    Feito o crud da categoria

--- back-end/src/routes/routes.js
+++
@@ -1,8 +0,0 @@
-
-import { Router } from "express";
-const routes = Router();
-import categoriaRoutes from "./categoriaRoutes.js"
-
-routes.use('/categoria', categoriaRoutes)
-
-export default routes
