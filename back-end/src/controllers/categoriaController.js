123294134c99d3060fb4945b750403733fa99ba2
NICOLAS GABRIEL COUTINHO <nicolas.coutinho3@senaisp.edu.br>
2026-05-15 13:15:24 -0300

    Crado a funcionalidade de editar uma categoria

--- back-end/src/controllers/categoriaController.js
+++ back-end/src/controllers/categoriaController.js
@@ -29,6 +29,36 @@ const categoriaController = {
                 error: error.message
             });
         }
+    },
+
+    editar: async (req, res) => {
+
+        try {
+
+            const id = req.params.id;
+
+            const { nome } = req.body;
+
+            const categoria = Categoria.alterar(
+                { nome },
+                id
+            );
+
+            const result = await categoriaRepository.editar(categoria);
+
+            res.status(200).json({
+                message: "Categoria atualizada com sucesso",
+                result
+            });
+
+        } catch (error) {
+
+            console.log(error);
+
+            res.status(500).json({
+                error: error.message
+            });
+        }
     }
 };
 
--- back-end/src/repositories/categoriaRepository.js
+++ back-end/src/repositories/categoriaRepository.js
@@ -16,6 +16,24 @@ const categoriaRepository = {
         const [result] = await connection.execute(sql, values);
 
         return result;
+    },
+
+    editar: async (categoria) => {
+
+        const sql = `
+            UPDATE categorias
+            SET Nome = ?
+            WHERE Id = ?
+        `;
+
+        const values = [
+            categoria.nome,
+            categoria.id
+        ];
+
+        const [result] = await connection.execute(sql, values);
+
+        return result;
     }
 };
 
