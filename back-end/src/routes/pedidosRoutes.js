b925a9119c8b71560ee5ee3a3235eaa0237e7768
DANIEL ALMEIDA LOVAGLIO <daniel.lovaglio2@senaisp.edu.br>
2026-05-22 08:21:55 -0300

    adicionando novas funcionalidades

--- back-end/src/routes/pedidosRoutes.js
+++ back-end/src/routes/pedidosRoutes.js
@@ -9,5 +9,9 @@ pedidoRoutes.get('/', pedidoController.selecionar);
 pedidoRoutes.get('/:id', pedidoController.selecionarPorId);
 pedidoRoutes.put('/:id', pedidoController.editar);
 pedidoRoutes.delete('/:id', pedidoController.deletar);
+pedidoRoutes.post("/:id/item", pedidoController.adicionarItem);
+pedidoRoutes.put("/item/:itemId", pedidoController.editarItem);
+pedidoRoutes.delete("/item/:itemId", pedidoController.deletarItem);
+
 
 export default pedidoRoutes;
