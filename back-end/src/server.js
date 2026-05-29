c15e0732a8b92a2a1750bdee47f0bfecfd58c150
LÍVIA DE SOUZA CAETANO <livia.caetano4@senaisp.edu.br>
2026-05-29 10:23:20 -0300

    :boom:configurar servidor express e rota pública de uploads

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,18 +1,37 @@
 import express from "express";
 import routes from "./routes/routes.js";
 import 'dotenv/config';
-import cors from 'cors'
+import cors from 'cors';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 
-app.use(cors())
+// Corrige caminho do __dirname no ESModules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// CORS
+app.use(cors());
+
+// JSON
 app.use(express.json());
 
+// Pasta uploads
+app.use(
+    '/uploads',
+    express.static(path.join(__dirname, '../uploads'))
+);
 
-app.use('/uploads', express.static('uploads'));
-
+// Rotas
 app.use('/', routes);
 
+// Servidor
 app.listen(process.env.SERVER_PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${process.env.SERVER_PORT}`);
+
+    console.log(
+        `Servidor rodando em http://localhost:${process.env.SERVER_PORT}`
+    );
+
 });
