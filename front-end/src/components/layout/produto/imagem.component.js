70569146e0b4b2500bec44e78b81787d7c91b8e6
LÍVIA DE SOUZA CAETANO <livia.caetano4@senaisp.edu.br>
2026-05-29 10:37:29 -0300

    :bug:fix melhorar estrutura e interação dos cards de produtos

--- front-end/src/components/layout/produto/imagem.component.js
+++ front-end/src/components/layout/produto/imagem.component.js
@@ -1,12 +1,35 @@
 export default function criarImagemProduto(produto){
+
     const img = document.createElement('img');
 
     img.className = 'card-img-top img-fluid';
-    img.alt = produto.name;
 
+    img.alt = produto.Nome;
+
+    // tamanho da imagem
     img.style.height = '360px';
-    img.style.objectFit = produto.image ? 'cover' : '';
+    img.style.width = '100%';
 
-    img.src = produto.image ? produto.image : '/public/default-character.png'
+    // mostra imagem inteira sem cortar
+    img.style.objectFit = 'contain';
+
+    // fundo branco
+    img.style.backgroundColor = '#fff';
+
+    // espaçamento interno
+    img.style.padding = '10px';
+
+    // imagem do produto
+    img.src = produto.Imagem
+        ? `http://localhost:8000/uploads/imagens/${encodeURIComponent(produto.Imagem)}`
+        : '/default-character.png';
+
+    // imagem padrão caso dê erro
+    img.onerror = () => {
+
+        img.src = '/default-character.png';
+
+    };
+
     return img;
 }
